a21f1c1a16c1549ffa9a1bedb82f9f47acbf476e
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-09 12:41:39 -0800

    first javascript loops

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
-for (let i =0; i < 1000000; i++){
-    console.log(i);
+
+ for (let i =1; i < 11; i++){
+     console.log(2**i);
+ }
+
+
+  
+let i=1;
+while(i<11){
+    console.log(2**i);
+    i++;
 }
